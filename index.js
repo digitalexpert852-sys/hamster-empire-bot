4797564c01d58edb295eab4d9188582e5ab2bb15
digitalexpert852-sys <digitalexpert852@gmail.com>
2026-05-26 15:59:53 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+
+const TOKEN = process.env.8688084678:AAHb9YZ848_vYOp2St4bdIzcyo8KDBsotcA;
+
+app.post("/webhook", async (req, res) => {
+    const update = req.body;
+
+    if (update.message) {
+        const chat_id = update.message.chat.id;
+        const text = update.message.text;
+        const name = update.message.from.first_name;
+
+        if (text === "/start") {
+            const message = `👋 Welcome ${name}!\n🐹 Hamster Empire Game`;
+
+            const url = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+            await axios.post(url, {
+                chat_id: chat_id,
+                text: message,
+                reply_markup: {
+                    inline_keyboard: [[
+                        {
+                            text: "🎮 Play Game",
+                            web_app: {
+                                url: "https://hamsterempire.kesug.com/"
+                            }
+                        }
+                    ]]
+                }
+            });
+        }
+    }
+
+    res.send("ok");
+});
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => console.log("Bot running"));
